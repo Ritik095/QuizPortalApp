8750ae8aabb7856acadbb8cc072ae6d5626d7b2e
Ritik Raushan <ritikraushan777@gmail.com>
2020-04-19 17:05:04 +0530

    first commit

---
+++ app/src/main/java/com/example/quizportelapp/select_topic.java
@@ -0,0 +1,14 @@
+package com.example.quizportelapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_topic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_topic);
+    }
+}
